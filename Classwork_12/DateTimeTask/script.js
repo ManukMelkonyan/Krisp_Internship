9cf51284e0667c0f75ab767955af75ba12f03710
ManukMelkonyan <vardan.melqonyan001@gmail.com>
2021-11-27 14:50:45 +0400

    Classwork 12 / DateTimeButton task Done

---
+++ Classwork_12/DateTimeTask/script.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('dateButton');
+let txtElem = document.getElementById('dateText');
+console.log(btn);
+console.log(txtElem);
+btn.addEventListener('click', function(){
+    let currentDate = new Date();
+    let formatted = `${currentDate.toLocaleDateString("en-US", {day: 'numeric'})}/${currentDate.toLocaleDateString("en-US", {month: 'short'})}/${currentDate.toLocaleDateString("en-US", {year: 'numeric'})}`;
+    txtElem.innerText = formatted;
+    txtElem.classList.toggle('hide-or-show');
+});
